e9aef8753f4f6584933729d9432e2d813d3d42d3
aaryanmx <aaryan.mx09@gmail.com>
2026-04-19 20:55:45 +0530

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Create server
+const server = http.createServer((req, res) => {
+    if (req.url.startsWith('/add')) {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        
+        const a = parseInt(url.searchParams.get('a'));
+        const b = parseInt(url.searchParams.get('b'));
+        
+        const result = add(a, b);
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ result }));
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("Addition Service Running");
+    }
+});
+
+// Start server
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
